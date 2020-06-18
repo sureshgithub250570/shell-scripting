48219ead17c0590561604e280faf0e55bb6e72fe
suresh <sureshsbalakrishna20@gmail.com>
2020-06-18 16:05:02 +0530

    changes done again Both ForeGround and Back Ground Colours Checking

--- 01-she-bang.sh
+++ 01-she-bang.sh
@@ -6,5 +6,7 @@ echo -e "Hello Good\nMorning"
 echo -e "\e[31mHello in Red Color"
 echo -e "\e[32mSuresh in Green color\e[0m"
 echo -e "With No Color"
+echo -e "Good\nMorning"
+echo -e "\e[31;34mBoth ForeGround and Back Ground\e[0m"
 
 
