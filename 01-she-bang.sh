885055847ec1ef176e139cd6301561cbebf87d34
suresh <sureshsbalakrishna20@gmail.com>
2020-06-18 15:02:23 +0530

    changes done again

--- 01-she-bang.sh
+++ 01-she-bang.sh
@@ -4,7 +4,7 @@ echo "This is Echo "
 echo this is second line
 echo -e "Hello Good\nMorning"
 echo -e "\e[31mHello in Red Color"
-echo -e "\e[32mSuresh in Green color"
-echo -e "\e[33m No color\e[0m"
+echo -e "\e[32mSuresh in Green color\e[0m"
+echo -e "With No Color"
 
 
