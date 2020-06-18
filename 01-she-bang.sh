6abee6e0486460172aecb8a7dfcf002cebe5e106
suresh <sureshsbalakrishna20@gmail.com>
2020-06-18 16:30:19 +0530

    changes done again Both ForeGround and Back Ground Colours Checking

--- 01-she-bang.sh
+++ 01-she-bang.sh
@@ -7,6 +7,13 @@ echo -e "\e[31mHello in Red Color"
 echo -e "\e[32mSuresh in Green color\e[0m"
 echo -e "With No Color"
 echo -e "Good\nMorning"
-echo -e "\e[31mBoth ForeGround and Back Ground\e[33;0m"
+echo -e "\e[31mBoth ForeGround and Back Ground\e[0m"
+a=100
+b=xyz
+c=abcd123
+d=true
+COURSE=DEVOPS
+echo -e "Welcome to ${COURSE} Training"
+echo -e"We offer linux scripting as part of ${COURSE} Training"
 
 
