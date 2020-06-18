a52a5b4b3e25c574c2f4d1e517cadbcab7390214
suresh <sureshsbalakrishna20@gmail.com>
2020-06-18 15:00:21 +0530

    color  disabled with comments

--- 01-she-bang.sh
+++ 01-she-bang.sh
@@ -5,6 +5,6 @@ echo this is second line
 echo -e "Hello Good\nMorning"
 echo -e "\e[31mHello in Red Color"
 echo -e "\e[32mSuresh in Green color"
-echo -e "\e[33 No color\e[0m"
+echo -e "\e[33m No color\e[0m"
 
 
